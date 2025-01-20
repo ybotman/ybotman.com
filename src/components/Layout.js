ca8df64363b9e69c95abc582c4cbe9d7d86af6df
ybotman <toby.balsley@gmail.com>
2025-01-20 12:45:53 -0500

    @/components/Header & Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Header from './Header';
-import Footer from "./Footer";
+import Header from '@/components/Header';
+import Footer from "@/components/Footer";
 
 function Layout({ children }) {
   return (
