48d70ec662be93e998b65f80aeba7332d42c8d85
ybotman <toby.balsley@gmail.com>
2025-03-26 11:32:02 -0400

    google tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,26 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
+const React = require("react");
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+  // Set the lang attribute
+  setHtmlAttributes({ lang: `en` });
+
+  // Add Google Analytics scripts
+  setHeadComponents([
+    <script
+      key="gtag-script"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-MR3J47RQKJ"
+    />,
+    <script key="gtag-inline">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MR3J47RQKJ');
+      `}
+    </script>,
+  ]);
+};
